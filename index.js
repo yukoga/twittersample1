8f13360f359af70708fd63532d3ab6bd09de2a9a
yukoga <yukoga@gmail.com>
2015-04-20 04:04:48 +0900

    add twitter code.

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var app = express();
-var twitterClient = require('twitter-js-client');
+var Twitter = require('twitter-js-client').Twitter;
+
+var error = function(err, response, body) {
+  console.log("ERROR: [%s]", err);
+};
+
+var success = function(data) {
+  console.log("Data [%s]", data);
+}
 
 var config = {
-  // "consumerKey": "",
-  // "consumerSecret": "",
+  "consumerKey": "EygchgUWpfTaJtKm1fqoEyLRu",
+  "consumerSecret": "QgC9rsjvpWFogEDCWP3s20qpQpL6SL70fVA7OXUtnPHfNk8673",
+  "accessToken": "",
+  "accessTokenSecret": "",
+  "callBackUrl": "https://desolate-stream-8656.herokuapp.com/"
 }
+
+try {
+var twitter = new Twitter(config);
+} catch (e) {
+  console.log(e);
+}
+console.log(twitter);
+var search = twitter.getSearch({'q': '#haiku', 'count': 10}, error, success);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World!' + search);
 });
 
 app.listen(app.get('port'), function() {
