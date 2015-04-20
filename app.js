89e3dc9841747257ba001d8eb6364d5d326a3669
yukoga <yukoga@gmail.com>
2015-04-21 01:27:21 +0900

    fix problem.

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 var app = require('express.io')(),
   config = require('./config/config'),
   glob = require('glob')
-  // mongoose = require('mongoose')
   ;
 
 /*
-mongoose.connect(config.db);
-var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + config.db);
-});
-
 var models = glob.sync(config.root + '/app/models/*.js');
 models.forEach(function (model) {
   require(model);
@@ -18,7 +11,8 @@ models.forEach(function (model) {
 */
 
 
-require('./config/express.io')(app, config);
+// require('./config/express.io')(app, config);
+require(config.root + '/config/express.io')(app, config);
 
 app.listen(config.port);
 
