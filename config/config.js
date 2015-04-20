26c7d36ef820ea535d096b066226614918642e35
yukoga <yukoga@gmail.com>
2015-04-21 03:53:48 +0900

    Update 2015-04-20T18:51:48.257Z

--- config/config.js
+++ config/config.js
@@ -2,29 +2,40 @@ var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development';
 
+var twitterKey = {
+  "consumerKey": "EygchgUWpfTaJtKm1fqoEyLRu",
+  "consumerSecret": "QgC9rsjvpWFogEDCWP3s20qpQpL6SL70fVA7OXUtnPHfNk8673",
+  // "accessToken": "2923537422-lbQWbLqlUvbqCBfpKwHvxW5V17MtbhZhTQfWsA5",
+  // "accessTokenSecret": "RiccwXHxdhvkRJCEsNSftWIoCytv1fhFMOQV5ctPSLzc0",
+  "callBackUrl": "https://desolate-stream-8656.herokuapp.com/"
+}
+
 var config = {
   development: {
+    twitter: twitterKey,
     root: rootPath,
     app: {
-      name: 'sample1'
+      name: 'TwitterSample1'
     },
     port: 3000,
     db: 'mongodb://localhost/sample1-development'
   },
 
   test: {
+    twitter: twitterKey,
     root: rootPath,
     app: {
-      name: 'sample1'
+      name: 'TwitterSample1'
     },
     port: 3000,
     db: 'mongodb://localhost/sample1-test'
   },
 
   production: {
+    twitter: twitterKey,
     root: rootPath,
     app: {
-      name: 'sample1'
+      name: 'TwitterSample1'
     },
     port: 3000,
     db: 'mongodb://localhost/sample1-production'
